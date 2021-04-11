2089f6486de78dad4bb6d3612a600d8a246536f4
Pierre Paillard <paillard.pierre@gmail.com>
2021-04-11 14:38:56 +0200

    fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularDirectiveExercice';
+  title = 'Angular Directive Exercice';
+  title2 = "Bonjour ! ";
+  isAdmin: boolean = false;
 }
